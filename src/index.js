e6b167f9b2cc06165dd9d17e53e06283102c995f
João Victor Ribeiro <victor.19ribeiro@gmail.com>
2025-04-10 22:18:45 -0300

    Projeto - verificador de palavras repetidas. tada, lógica inicial implementada..

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const caminhoArquivo = process.argv;
+const link = caminhoArquivo[2];
+
+fs.readFile(link, 'utf-8', (erro, texto) => {
+    verificaPalavrasDuplicadas(texto);
+});
+
+// criar um array com as palavras - OK
+// contar as ocorrências - OK
+// montar um objeto com o resultado
+
+// {
+//     "web": 5,
+//     "computador": 4,
+// }
+
+function verificaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+    // objeto[propriedade] = valor;
+    listaPalavras.forEach((palavra) => {
+        resultado[palavra] = (resultado[palavra] || 0) + 1;
+    })
+    console.log(resultado);
+
+};
