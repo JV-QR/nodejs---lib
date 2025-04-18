cf52461876a08000bb5d5e10b01b147cd82030af
João Victor Ribeiro <victor.19ribeiro@gmail.com>
2025-04-18 12:26:04 -0300

    Refatora index.js para melhorar a organização do código e implementar tratamento de erros no readFile.

--- src/index.js
+++ src/index.js
@@ -4,28 +4,24 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-    quebraEmParagrafos(texto);
-    // verificaPalavrasDuplicadas(texto);
+    if(erro) {
+        console.log('qual é o erro?', erro.code);
+        return;
+    }
+    contaPalavras(texto);
 });
 
-// criar um array com as palavras - OK
-// contar as ocorrências - OK
-// montar um objeto com o resultado - ok
-
-// {
-//     "web": 5,
-//     "computador": 4,
-// }
-
-// organizar os paragafos
-
-function quebraEmParagrafos(texto) {
-    const paragrafos = texto.toLowerCase().split('\n');
+function contaPalavras(texto) {
+    const paragrafos = extraiParagrafos(texto);
     const contagem = paragrafos.flatMap((paragrafo) => {
         if(!paragrafo) return [];
         return verificaPalavrasDuplicadas(paragrafo);
     })
     console.log(contagem);
+};
+
+function extraiParagrafos(texto) {
+    return texto.toLowerCase().split('\n');
 };
 
 function limpaPlavras(palavra) {
@@ -35,7 +31,6 @@ function limpaPlavras(palavra) {
 function verificaPalavrasDuplicadas(texto) {
     const listaPalavras = texto.split(' ');
     const resultado = {};
-    // objeto[propriedade] = valor;
     listaPalavras.forEach((palavra) => {
         if(palavra.length >= 3) {
             const palavraLimpa = limpaPlavras(palavra);
