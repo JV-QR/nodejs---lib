56e3d474816fefc7472a4c79a3f625e828894274
João Victor Ribeiro <victor.19ribeiro@gmail.com>
2025-04-21 14:20:46 -0300

    Adiciona função de tratamento de erros para leitura de arquivos, separando lógica de erro e melhorando a gestão de exceções com 'new Error'

---
+++ src/erros/funcoesErro.js
@@ -0,0 +1,12 @@
+function trataErros(erro) {
+    if(erro.code === 'ENOENT') {
+        throw new Error('Arquivo não encontrado');
+        // return 'Arquivo não encontrado';
+    } else if(erro.code === 'EISDIR') {
+        throw new Error('Esperado um arquivo, mas foi encontrado um diretório');
+    } else {
+        return 'Erro na aplicação';
+    }
+};
+
+module.exports = trataErros;
